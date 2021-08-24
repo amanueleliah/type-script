50162f60f8670c4213b8726c02e2f81b511b0b72
amanueleliah <74117550+amanueleliah@users.noreply.github.com>
2021-08-24 10:46:45 -0500

    First Commit

---
+++ sandbox.ts
@@ -0,0 +1,8 @@
+var character = 'Amanuel Gebru';
+console.log(character);
+var inputs = document.querySelectorAll('input');
+console.log(inputs);
+
+inputs.forEach(input => {
+    console.log(input);
+});
